774fe898bdf45bc979492acf3ee70f79dd8b4d8c
Jota Carvalho <joao.carvalho@unblindgroup.com>
2022-09-07 09:25:53 +0100

    Add: - Half-way step 1 - Better active page on sidebar - Remove sidebar_controller.js from editor - Fix Draggable Items on new section

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
 
   private
 
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    admin_deals_path
+  end
+
   def set_is_app
     @is_app = true
   end
