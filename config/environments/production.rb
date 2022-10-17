01a928d6e69cbbf9b288c9bf8cef7f7a9f013105
Jota Carvalho <joao.carvalho@unblindgroup.com>
2022-10-17 13:57:02 +0100

    Change url from unbliind to powerdeal.pt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_controller.default_url_options = { host: 'www.unbliind.com' }
-  config.action_mailer.default_url_options = { host: "www.unbliind.com" }
-  config.action_mailer.asset_host = 'https://www.unbliind.com'
+  config.action_controller.default_url_options = { host: 'www.powerdeal.pt' }
+  config.action_mailer.default_url_options = { host: "www.powerdeal.pt" }
+  config.action_mailer.asset_host = 'https://www.powerdeal.pt'
 
 end
